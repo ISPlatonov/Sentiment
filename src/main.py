9d30af684f7bdc604bb1004936aebd0c6485a346
isplatonov <isplatonov@miem.hse.ru>
2021-03-18 02:03:44 +0300

    lemmatization is added

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from typing import List, Dict
 from fastapi import FastAPI
 
 from .config import CLASS_DICT
-from .sentiment import evaluate_sentiment, get_nsubj
+from .sentiment import evaluate_sentiment
 
 app = FastAPI()
 
@@ -21,19 +21,19 @@ def get_best_class(request: Dict[str, List[str]]):
 
 # new signal
 # to evaluate scores for the texts
-# related to given nsubj
+# related to given keywords
 @app.post("/sentiment/rel/scores")
-def get_relative_score(texts: List[str], nsubj: List[str]):
-    return {"results": evaluate_sentiment(texts, nsubj), "nsubj": nsubj}
+def get_relative_score(texts: List[str], keywords: List[str]):
+    return {"results": evaluate_sentiment(texts, keywords), "keywords": keywords}
 
 
 # new signal
 # to return the most related classes
-# for the texts related to given nsubj
+# for the texts related to given keywords
 @app.post("/sentiment/rel/scores/best")
-def get_best_relative_class(texts: List[str], nsubj: List[str]):
-    results = evaluate_sentiment(texts, nsubj)
-    return {"results": [max(text_res.items(), key=lambda x: x[1])[0] for text_res in results], "nsubj": nsubj}
+def get_best_relative_class(texts: List[str], keywords: List[str]):
+    results = evaluate_sentiment(texts, keywords)
+    return {"results": [max(text_res.items(), key=lambda x: x[1])[0] for text_res in results], "keywords": keywords}
 
 
 @app.get("/sentiment/classes")
